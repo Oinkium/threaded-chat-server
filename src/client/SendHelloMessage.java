e23b44eacaed6d312735594f649d089c2078c47e
John Gowers <wjg27@bath.ac.uk>
2016-02-29 16:29:25 +0000

    Added some files

---
+++ src/client/SendHelloMessage.java
@@ -0,0 +1,14 @@
+package client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SendHelloMessage {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
